01fcfcf38d0f0ed06bfc0ed219992cfdf4fc36fc
Alejandro Salazar Mejía <alejosalazarmejia.2000@gmail.com>
2024-08-18 14:26:12 -0500

    Update UserDao.java

--- app/src/main/java/alejandro/salazar/mejia/dao/UserDao.java
+++ app/src/main/java/alejandro/salazar/mejia/dao/UserDao.java
@@ -98,6 +98,16 @@ public class UserDao {
 
     public void addUserTag(UserTagEvent userTagEvent) throws Exception {
 
+        // Send the event to Kafka
+        Instant time = userTagEvent.getTime();
+        String timeBucket = time.atZone(ZoneOffset.UTC).withSecond(0).withNano(0).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        String value = objectMapper.writeValueAsString(userTagEvent);
+        producer.send(new ProducerRecord<>(TOPIC, timeBucket, value), (metadata, exception) -> {
+            if (exception != null) {
+                log.error("Error while sending message to Kafka", exception);
+            }
+        });
+
         // Cookie is the unique identifier for the user
         Key key = new Key(NAMESPACE, SET, userTagEvent.getCookie());
         Policy readPolicy = new Policy(client.readPolicyDefault);
@@ -165,16 +175,6 @@ public class UserDao {
             }
 
         }
-
-        // Send the event to Kafka
-        Instant time = userTagEvent.getTime();
-        String timeBucket = time.atZone(ZoneOffset.UTC).withSecond(0).withNano(0).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-        String value = objectMapper.writeValueAsString(userTagEvent);
-        producer.send(new ProducerRecord<>(TOPIC, timeBucket, value), (metadata, exception) -> {
-            if (exception != null) {
-                log.error("Error while sending message to Kafka", exception);
-            }
-        });
 
     }
 
